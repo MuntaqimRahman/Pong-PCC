294ac6869ae8fe8c30b8cc6b3297e8e79d2e9fa6
Muntaqim Rahman <rmuntaqim@yahoo.com>
2019-06-27 17:42:19 -0400

    Unit Test Code and Results

---
+++ Test-Information/Ball.Test/BallTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pong_PCC;
+
+namespace Ball.Test
+{
+    [TestClass]
+    public class BallTest
+    {
+        [TestMethod]
+        public void yDetection_DifferentYPositionStarts_ReturnsEqualValue()
+        {
+            //Expected values and ball initialized with current settings
+            float[] expectedValues = new float[6] {126,34,58,150,242,334}; //Expected Values derived from theory and experimentation
+            Pong_PCC.Ball ball = new Pong_PCC.Ball(640, 480, 10, 10, 5, 60);
+
+            //yDistance and xDistance constant - always starting from xPosition 618
+            ball.YDistance = 4;
+            ball.XDistance = 3 * (480 / 365);
+            ball.XPosition = 618;
+            
+
+            //Testing different y values
+            for (int i = 0; i < 6; i ++)
+            {
+                ball.YPosition = 10+i*92;
+                ball.yDetection();
+                float actualPosition = ball.PredictedYPosition;
+
+                Assert.AreEqual(expectedValues[i], actualPosition);
+                
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Test-Information/Ball.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ball.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ball.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cea0ffdf-0d20-4314-8d9b-a68e9b61e781")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
